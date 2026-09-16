20827a7aee32698e3beea2c0f090008cad26d55e
IllianiBird <danielfarbrace@icloud.com>
2026-09-16 13:13:13 -0400

    Removed dirty edits

--- MekHQ/scripts/shell.sh
+++ MekHQ/scripts/shell.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2005-2026 The MegaMek Team. All Rights Reserved.
+# Copyright (C) 2005-2025 The MegaMek Team. All Rights Reserved.
 #
 # This file is part of MekHQ.
 #
@@ -31,8 +31,4 @@
 # <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
 # affiliated with Microsoft.
 
-# Resolve the directory this script lives in and run from there.
-SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-cd "$SCRIPT_DIR" || exit 1
-
-exec "$SCRIPT_DIR/bin/MekHQ" "$@"
+./bin/MekHQ "$@"
